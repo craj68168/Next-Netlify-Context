c8357ea32abd294727d3ee4a1ffd55299093fec3
craj68168 <craj68168@gmail.com>
2022-09-05 16:33:27 +0545

    nwtlify funtion with identify

---
+++ functions/guides.js
@@ -0,0 +1,12 @@
+exports.guides = async (event, context) => {
+  if (context.clientContext.user) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ name: "guides data" }),
+    };
+  }
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ msg: "Please login first" }),
+  };
+};
