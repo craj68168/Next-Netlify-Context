710c16bdc77d38430eb82a10e4693378e7f627ab
craj68168 <craj68168@gmail.com>
2022-09-08 17:19:01 +0545

    styled component implemented on remaining home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import styles from '../styles/Home.module.css'
+import styled from "styled-components"
+
+const Div = styled.div`max-width: 760px;
+margin: 40px auto;
+letter-spacing: 1px;
+line-height: 1.6em;`
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <Div>
        <h2>Welcome to Gaming Vibes</h2>
       <div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deleniti rem aspernatur odit hic autem neque repellat alias? Debitis veniam inventore ipsum similique quos animi ipsa asperiores fuga dolor id.</p>
       </div>
-    </div>
+    </Div>
   )
 }
